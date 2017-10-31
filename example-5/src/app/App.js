a7f8f2122360195ed870728bae016ab122e943c2
David de Lusenet <lusenet@momkai.com>
2017-10-31 11:45:13 +0100

    Added example-5 setup

--- example-5/src/app/App.js
+++ example-5/src/app/App.js
@@ -10,11 +10,24 @@ class App extends React.Component {
 
     // Bind functions
     this.addTodoToState = this.addTodoToState.bind(this);
+    this.deleteTodoFromState = this.deleteTodoFromState.bind(this);
 
     // Set initial state
     this.state = {
       todos: []
     }
+  }
+
+  componentWillMount() {
+    if (localStorage.getItem('example-5-todos')) {
+      this.setState({
+        todos: JSON.parse(localStorage.getItem('example-5-todos'))
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('example-5-todos', JSON.stringify(this.state.todos));
   }
 
   addTodoToState(newTodo) {
@@ -23,10 +36,25 @@ class App extends React.Component {
     }));
   }
 
+  deleteTodoFromState(id) {
+    const UpdatedTodos = this.state.todos;
+
+    for (let i = 0; i < UpdatedTodos.length; i++) {
+      if (UpdatedTodos[i].id === id) {
+        UpdatedTodos.splice(i, 1);
+        return;
+      }
+    }
+
+    this.setState({
+      todos: UpdatedTodos
+    });
+  }
+
   render() {
     return [
       <TodoForm key="TodoForm" addTodoToState={this.addTodoToState} />,
-      <TodoList key="TodoList" todos={this.state.todos} />
+      <TodoList key="TodoList" todos={this.state.todos} deleteTodoFromState={this.deleteTodoFromState} />
     ];
   }
 }
--- example-5/src/app/components/TodoList/TodoList.js
+++ example-5/src/app/components/TodoList/TodoList.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './TodoList.scss';
 
-const TodoList = (props) => (
-  <ul styleName="TodoList">
-    {props.todos.map(todo => (
-      <li styleName="TodoList__item" key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
-);
+// Import child components
+import TodoListItem from './TodoListItem/TodoListItem';
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Bind functions
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  deleteTodo(id) {
+    this.props.deleteTodoFromState(id);
+  }
+
+  render() {
+    return (
+      <ul styleName="TodoList">
+        {this.props.todos.map(todo => (
+          <TodoListItem key={todo.id} {...todo} deleteTodo={this.deleteTodo} />
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default TodoList;
