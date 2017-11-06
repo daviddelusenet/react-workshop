b846135dbeadaa71e4ac60a9a9d118765d65c7b4
David de Lusenet <lusenet@momkai.com>
2017-11-06 09:12:21 +0100

    Added example 7 first setup

---
+++ example-7/src/app/App.js
@@ -0,0 +1,18 @@
+import React              from 'react';
+
+// Import child components
+import AnimationShowcase  from 'AnimationShowcase/AnimationShowcase';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AnimationShowcase />
+    );
+  }
+}
+
+export default App;
---
+++ example-7/src/index.js
@@ -0,0 +1,17 @@
+// Import React stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import shared libraries
+import 'gsap';
+
+// Import shared styles
+import './styles/global/global.scss';
+
+// Import application
+import App from './app/App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#main')
+);
