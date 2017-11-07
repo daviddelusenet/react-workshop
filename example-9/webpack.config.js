5e915f62307d420639c32e627a790e06bcd0b85d
David de Lusenet <lusenet@momkai.com>
2017-11-07 08:53:31 +0100

    Changed example-8 to example-9 + started with example-9 styled components setup

---
+++ example-9/webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+const context = resolve(__dirname, './src');
+
+module.exports = {
+  context,
+  entry: {
+    app: ['babel-polyfill', './index.js']
+  },
+  devServer: {
+    contentBase: resolve(__dirname, './dist'),
+    publicPath: '/js/',
+    port: 3000,
+    historyApiFallback: true
+  },
+  output: {
+    path: resolve(__dirname, './dist/js'),
+    filename: '[name].min.js',
+    publicPath: '/js/'
+  },
+  resolve: {
+    modules: ['node_modules', 'src/app/components']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: false,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: () => {
+                return [
+                  require('autoprefixer')({
+                    browsers: 'last 10 versions'
+                  })
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              includePaths: ['node_modules/foundation-sites/scss', 'src/styles']
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
